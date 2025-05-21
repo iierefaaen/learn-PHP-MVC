6c1eaef176d44f9f83a4d0581303d810410e969b
iierefaaen <iierefaaen@gmail.com>
2025-05-21 22:47:08 +0700

    add error UI for Students

--- app/controllers/admin/Students.php
+++ app/controllers/admin/Students.php
@@ -16,10 +16,19 @@ class Students extends Controller {
         $this->view("templates/footer");
     }
 
-    public function detail($id){
+    public function detail($id = null){
         $data["title"] = "Detail Mahasiswa";
 
         $data["student"] = $this->model("Student_Model")->getStudentByID($id);
+
+        if ( $data["student"] == false || $id == null ){
+            // if data not found, PDO return false
+            $data["title"] = "404 - Data Tidak Ditemukan";
+            $this->view("templates/header", $data);
+            $this->view("Students/error");
+            $this->view("templates/footer");
+            exit;
+        }
 
         $this->view("templates/header", $data);
         $this->view("Students/student_detail", $data);
@@ -31,11 +40,12 @@ class Students extends Controller {
             if ( isset($_POST["add"]) ){
                 $result = $this->model("Student_Model")->add_student($_POST, $_FILES["photo"]);
 
-                // TODO : replace with alert
                 if ( $result === 1 ){
-                    echo "BERHASIL MENAMBAHKAN DATA";
+                    header("Location:" . BASE_URL . "students?title=Berhasil&message=Data Berhasil Ditambahkan&type=success");
+                    exit;
                 } else {
-                    echo "GAGAL MENAMBAHKAN DATA";
+                    header("Location:" . BASE_URL . "students?title=Gagal&message=Data Gagal Ditambahkan&type=danger");
+                    exit;
                 }
             }
         }
@@ -48,7 +58,7 @@ class Students extends Controller {
     }
     
 
-    public function edit($id) {
+    public function edit($id = null) {
         if ( $_SERVER['REQUEST_METHOD'] === "POST" ){
             if ( isset($_POST["edit"]) ){
                 if ( $_FILES["photo"]["error"] === 4 ) {
@@ -59,7 +69,6 @@ class Students extends Controller {
                 }
 
 
-                // TODO : replace with alert
                 if ( $result === 1 ){
                     header("Location:" . BASE_URL . "students?title=Berhasil&message=Data Berhasil Diperbarui&type=success");
                     exit;
@@ -67,13 +76,25 @@ class Students extends Controller {
                     header("Location:" . BASE_URL . "students?title=Peringatan&message=Tidak Ada Data Yang Diubah&type=warning");
                     exit;
                 } else {
-                    header("Location:" . BASE_URL . "students?title=Gagal&message=Data Gagal diperbarui&type=danger");
+                    header("Location:" . BASE_URL . "students?title=Gagal&message=Data Gagal Diperbarui&type=danger");
                     exit;
                 }
             }
         }
 
         $data["student"] = $this->model("Student_Model")->getStudentByID($id);
+
+        if ( $data["student"] == false || $id == null){
+            // if data not found, PDO return false
+            $data["title"] = "404 - Data Tidak Ditemukan";
+            $this->view("templates/header", $data);
+            $this->view("Students/error");
+            $this->view("templates/footer");
+            exit;
+        }
+
+
+
         $data["title"] = "Edit Data mahasiswa";
 
         $this->view("templates/header", $data);
@@ -81,24 +102,31 @@ class Students extends Controller {
         $this->view("templates/footer");
     }
 
-    public function delete($id, $bool = null) {
+    public function delete($id = null, $bool = null) {
         // @SOFT DELETE ONLY
 
         if ( $bool === "yes"){
             $result =  $this->model("Student_Model")->deleteStudentData($id);
 
-            // TODO : replace with alert
                 if ( $result === 1 ){
-                    // echo "BERHASIL MENGHAPUS DATA";
-                    // TODO : add alert
-                    header("Location:" . BASE_URL . "students");
+                    header("Location:" . BASE_URL . "students?title=Berhasil&message=Data Berhasil Dihapus&type=success");
                     exit;
                 } else {
-                    echo "GAGAL MENGHAPUS DATA";
+                    header("Location:" . BASE_URL . "students?title=Gagal&message=Data Gagal Dihapus&type=danger");
+                    exit;
                 }
         }
 
         $data["student"] = $this->model("Student_Model")->getStudentByID($id);
+
+        if ( $data["student"] == false || $id == null ){
+            // if data not found, PDO return false
+            $data["title"] = "404 - Data Tidak Ditemukan";
+            $this->view("templates/header", $data);
+            $this->view("Students/error");
+            $this->view("templates/footer");
+            exit;
+        }
         
         $data["title"] = "Hapus Data mahasiswa";
         $this->view("templates/header", $data);
---
+++ app/views/admin/Students/error.php
@@ -0,0 +1,8 @@
+<div class="flex-fill d-flex align-items-center justify-content-center">
+    <div class="text-center">
+      <h1 class="display-1 fw-bold text-danger">404</h1>
+      <p class="fs-3"> <span class="text-danger">Oops!</span> Halaman tidak ditemukan.</p>
+      <p class="lead">Halaman yang kamu cari mungkin sudah dihapus atau tidak tersedia.</p>
+      <a href="<?= BASE_URL; ?>" class="btn btn-primary">Kembali ke Beranda</a>
+    </div>
+  </div>
