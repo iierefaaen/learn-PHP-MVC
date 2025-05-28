3a7bf0c3e35909b498cab2f269ca9f193a8212ad
iierefaaen <iierefaaen@gmail.com>
2025-05-28 18:23:53 +0700

    refactor App.php

--- app/core/App.php
+++ app/core/App.php
@@ -4,98 +4,77 @@ class App {
     protected $controller = "Dashboard";
     protected $method = "index";
     protected $params = [];
+
+    protected $adminControllerPath = "../app/controllers/admin/";
+    protected $authControllerPath = "../app/controllers/Auth/Auth.php";
+
     public function __construct() {
+        if (session_status() === PHP_SESSION_NONE) session_start();
 
-        if ( session_status() === PHP_SESSION_NONE ) session_start();
-        if ( !isset($_SESSION["login"]) || $_SESSION["login"] !== "LOGIN" ) {
-            // header("Location : " . BASE_URL . "auth/login");
-            $this->controller = "auth";
-            $this->method = "login";
+        $url = $this->parseURL();
 
-            require_once("../app/controllers/Auth/Auth.php");
-            $this->controller = new $this->controller;
-            call_user_func_array([$this->controller, $this->method], $this->params);
-            exit;
-        }
-        
-        
-        
-        $url = $this->parse_URL();
-
-
-        if ( $url[0] === "auth" && $url[1] === "logout") {
-            $this->controller = "auth";
-            $this->method = "logout";
-
-            require_once("../app/controllers/Auth/Auth.php");
-            $this->controller = new $this->controller;
-            call_user_func_array([$this->controller, $this->method], $this->params);
-            exit;
+        if (!$this->isLoggedIn()) {
+            $this->runAuth("login");
+            return;
         }
 
-        // controllers
-        if ( file_exists("../app/controllers/admin/" . ucfirst($url[0]) . ".php")){
-            //
-            $this->controller = ucfirst($url[0]);
-            unset($url[0]);
-        }
-        require_once "../app/controllers/admin/" . ucfirst($this->controller) . ".php";
-        $this->controller = new $this->controller;
-
-        // method
-        if ( isset($url[1]) ){
-            if ( method_exists($this->controller, $url[1])){
-                $this->method = $url[1];
-                unset($url[1]);
-            }
+        if ($this->isLogoutRequest($url)) {
+            $this->runAuth("logout");
+            return;
         }
 
-        // parameter
-        if ( !empty($url) ){
-            $this->params = array_values($url);
-        }
+        $this->handleController($url);
+        $this->handleMethod($url);
+        $this->handleParams($url);
 
-
-        // TODO
-        // if ( !$this->isLogedIn() ){
-        //     $this->controller = "auth";
-        //     $this->method = "login";
-            
-        //     require_once("../app/controllers/Auth/Auth.php");
-        //     $this->controller = new $this->controller;
-        //     // call_user_func_array([$this->controller, $this->method], $this->params);
-        //     // exit;
-        // }
-        // : TODO
-
-
-        // jalankan controller, method, kirim params
         call_user_func_array([$this->controller, $this->method], $this->params);
     }
 
-    public function parse_URL(){
-        if ( isset($_GET["url"]) ) {
-            // remove / in strat and end with trim()
+    private function parseURL() {
+        if (isset($_GET["url"])) {
             $url = trim($_GET["url"], "/");
             $url = filter_var($url, FILTER_SANITIZE_URL);
-            $url = explode("/", $url);
-            return $url;
-        } else {
-            return ["home", "index"];
+            return explode("/", $url);
         }
-        
+        return ["home", "index"];
     }
 
-    // TODO :
-    public function isLogedIn() {
-        if ( !isset($_SESSION["login"]) ) {
-            // header("Location: http://localhost:8080/auth/login");
-            // exit;
-            // $this->controller = "auth";
-            // $this->method = "login";
-            return false;
-        }
-        return true;
+    private function isLoggedIn() {
+        return isset($_SESSION["login"]) && $_SESSION["login"] === "LOGIN";
+    }
 
+    private function isLogoutRequest($url) {
+        return isset($url[0], $url[1]) && $url[0] === "auth" && $url[1] === "logout";
+    }
+
+    private function runAuth($method) {
+        require_once $this->authControllerPath;
+        $controller = new Auth();
+        call_user_func([$controller, $method]);
+        exit;
+    }
+
+    private function handleController(&$url) {
+        $controllerName = ucfirst($url[0] ?? $this->controller);
+        $controllerFile = $this->adminControllerPath . $controllerName . ".php";
+
+        if (file_exists($controllerFile)) {
+            $this->controller = $controllerName;
+            unset($url[0]);
+        }
+
+        require_once $this->adminControllerPath . $this->controller . ".php";
+        $this->controller = new $this->controller;
+    }
+
+    private function handleMethod(&$url) {
+        if (isset($url[1]) && method_exists($this->controller, $url[1])) {
+            $this->method = $url[1];
+            unset($url[1]);
+        }
+    }
+
+    private function handleParams($url) {
+        $this->params = array_values($url);
     }
 }
